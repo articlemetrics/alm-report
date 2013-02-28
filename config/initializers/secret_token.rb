a03ea01b2cd516b24b05740cc92b77160d36ddca
Jen Song <jsong@plos.org>
2013-02-28 13:09:07 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlmReport::Application.config.secret_token = '3be9f1f0629ccb72c86bd848e3cab7ae53186c57ba02d33e1849be0568c7875078183aea1dc7f14679e3dbaf6c47aa3fb3a25d11a59528c00f436a0980c5c15e'
